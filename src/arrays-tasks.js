a34c9a40c005fed16aeff1d639f01550d3b630e5
Mikhail Vezhnovets <mvezhnovets@gmail.com>
2024-12-07 21:01:20 +0300

    feat: add solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -121,8 +121,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const result = arr.reduce((sum, current) => sum + current, 0);
+
+  if (result === 0 && arr.length === 0) {
+    return 0;
+  }
+  return Math.round((result / arr.length) * 100) / 100;
 }
 
 /**
